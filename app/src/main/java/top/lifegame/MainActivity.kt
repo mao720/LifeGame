8dc4e88b5237fe9e4853e33737d94f86a151ce71
maojunfeng <1009935357@qq.com>
2020-08-18 09:32:04 +0800

    feature-旋转动画，每秒动6°

--- app/src/main/java/top/lifegame/MainActivity.kt
+++ app/src/main/java/top/lifegame/MainActivity.kt
@@ -1,8 +1,12 @@
 package top.lifegame
 
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.view.animation.LinearInterpolator
+import android.widget.Button
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.gyf.immersionbar.ktx.immersionBar
 import top.lifegame.ui.main.MainFragment
 import top.lifegame.ui.main.MainViewModel
@@ -19,6 +23,19 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+        val btnButton = findViewById<Button>(R.id.btn_button)
+        ValueAnimator.ofInt(
+            0,
+            100000
+        ).apply {
+            duration = 100000000
+            interpolator = LinearInterpolator()
+            addUpdateListener { animation ->
+                val animatedValue: Int = animation.animatedValue as Int
+                btnButton.rotation = -(animatedValue * 6).toFloat()
+            }
+            start()
         }
     }
 }
